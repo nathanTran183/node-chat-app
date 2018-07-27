c0157db7bb025b7207653bb6b54439ea6d1fd82f
Nathan Tran <phuctd1803@gmail.com>
2018-07-27 18:47:55 +0700

    Greet new user, alert others

--- server/server.js
+++ server/server.js
@@ -13,14 +13,37 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('new user connection');
 
+	socket.emit('newMessage', {
+		from: 'Admin',
+		text: 'Welcome to the chat app',
+		createAt: new Date().getTime()	
+	});
+
+	socket.broadcast.emit('newMessage', {
+		from: 'Admin',
+		text: 'New user joined',
+		createAt: new Date().getTime()
+	});
+
 	socket.on('createMessage', (message) => { 
 		console.log('createMessage', message);
+
+		// send to all 
 		io.emit('newMessage', {
 			from: message.from,
 			text: message.text,
 			createAt: new Date().getTime()
-		})
+		});
+
+		//only receive from other connection/users
+		// socket.broadcast.emit('newMessage', {
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createAt: new Date().getTime()
+		// });
 	});
+
+
 
 	socket.on('disconnect', () => {
 			console.log('User was disconnected!');
